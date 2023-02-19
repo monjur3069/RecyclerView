7c631a2591e48b3c9b387a9c623869dd95184395
Monjur Ahmed Akash <monjur3069@gmail.com>
2023-02-19 11:27:06 +0600

    Inital Commit

---
+++ RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/Employee.kt
@@ -0,0 +1,32 @@
+package com.example.recyclerviewexample
+
+data class Employee(
+    val id: Int,
+    val name: String,
+    val designation: String
+)
+
+val employeeList = mutableListOf(
+    Employee(1,"ABC","CEO"),
+    Employee(2,"FJA","FAR"),
+    Employee(3,"FFA","VMM"),
+    Employee(4,"ERU","QOI"),
+    Employee(5,"UTI","TIP"),
+    Employee(6,"AKY","GHJ"),
+    Employee(7,"ITO","JKF"),
+    Employee(8,"MVN","RQU"),
+    Employee(9,"QRU","HKF"),
+    Employee(10,"QPR","URI"),
+    Employee(11,"QRI","AFF"),
+    Employee(12,"VMZ","URQ"),
+    Employee(13,"QTR","GHJ"),
+    Employee(14,"NVF","QRY"),
+    Employee(15,"IQR","GVI"),
+    Employee(16,"YUI","JFJ"),
+    Employee(17,"ITF","TUR"),
+    Employee(18,"RJI","JFS"),
+    Employee(19,"JAF","TOU"),
+    Employee(20,"OPW","VZJ"),
+    Employee(21,"PWI","YYJ"),
+    Employee(22,"ITO","VJK"),
+)
---
+++ RecyclerViewExample/app/src/main/java/com/example/recyclerviewexample/EmployeeAdaptar.kt
@@ -0,0 +1,40 @@
+package com.example.recyclerviewexample
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewexample.databinding.EmployeeRowBinding
+
+class EmployeeAdaptar : ListAdapter<Employee, EmployeeAdaptar.EmployeeViewHolder>(EmployeeDiffUtil()) {
+    class EmployeeViewHolder(val binding: EmployeeRowBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(employee: Employee){
+            binding.employee = employee
+        }
+    }
+
+    class  EmployeeDiffUtil : DiffUtil.ItemCallback<Employee>(){
+        override fun areItemsTheSame(oldItem: Employee, newItem: Employee): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Employee, newItem: Employee): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmployeeViewHolder {
+        val binding = EmployeeRowBinding.inflate(
+            LayoutInflater.from(parent.context),parent,false
+        )
+        return EmployeeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EmployeeViewHolder, position: Int) {
+        val employee = getItem(position)
+        holder.bind(employee)
+    }
+}
